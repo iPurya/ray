4f5dbbac10353abb5ec6f6681a76a358b04fbace
Purya <purya.jafari@hotmail.com>
2023-01-11 12:08:12 +0330

    bot initialize

---
+++ vpn.py
@@ -0,0 +1,20 @@
+from telebot import TeleBot
+from redis import Redis
+import config
+
+redis = Redis(db=5 , charset="utf-8", decode_responses=True)
+
+bot = TeleBot(config.BOT_TOKEN)
+print(f"Bot Started @{bot.get_me().username}")
+
+@bot.message_handler()
+def msg_handler(msg):
+    text = str(msg.text or msg.caption or '')
+    chat_id = msg.chat.id
+    from_id = msg.from_user.id
+    if from_id != 590740002: return
+    print(chat_id,text)
+    if text == '/start':
+        bot.reply_to(msg,"Hello World!")
+    
+bot.infinity_polling(skip_pending=True)
